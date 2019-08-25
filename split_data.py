f722daaf99b78dc2b505efddc9ae81654df7ff05
Sushant Bansal <sushant@uchicago.edu>
2019-08-25 02:14:13 -0500

    Added docs and now it returns the df as well

--- split_data.py
+++ split_data.py
@@ -3,7 +3,22 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 
-def split_data(file_path = None, df = None, prefix="", seed=1):
+def split_data(file_path = None, df = None, prefix="", seed=1, ret = 0):
+    
+    """
+    Splits data into Train, Test and Validation and saves them in the ProcessedData folder
+    
+    Args:
+        file_path: Input Data in csv format
+        df: Dataframe can be sent instead of a file
+        prefix: Prefix for file names like:
+            [Prefix]_train.csv, [Prefix]_test.csv, [Prefix]_val.csv
+        seed: seed to randomize the split
+        ret: set to 1 if you want the data_frames_back as well 
+    
+    Returns:
+        Tuple of Train, Val and Test pandas dataframes.
+    """
     
     if file_path is not None:
         data = pd.read_csv(file_path)
@@ -21,3 +36,6 @@ def split_data(file_path = None, df = None, prefix="", seed=1):
     train.to_csv(dir_prefix+prefix+"_train.csv", index=False, header=False)
     val.to_csv(dir_prefix+prefix+"_val.csv", index=False, header=False)
     test.to_csv(dir_prefix+prefix+"_test.csv", index=False, header=False)
+    
+    if ret == 1:
+        return train, val, test
